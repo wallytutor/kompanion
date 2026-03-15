fa0859ab0fe265691503890927f24cd3519827d8
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-03-15 11:41:50 +0100

    Initializing gh-pages branch

---
+++ .ipython/profile_default/startup/00-autoreload.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+import sys
+
+print(f"\n\033[32mKOMPANION IPYTHON\033[0m")
+print(f"\033[34mUsing: {Path(sys.executable).as_posix()}\033[0m\n")
+
+try:
+    from IPython import get_ipython
+
+    if (ipython := get_ipython()) is not None:
+        ipython.run_line_magic("load_ext", "autoreload")
+        ipython.run_line_magic("autoreload", "2")
+        print("\033[34m- Autoreload enabled (mode 2)\033[0m")
+except Exception as e:
+    print(f"\033[31m- Could not enable autoreload: {e}\033[0m")
+
+# For now keep it off, as we are working on majordome.exe.
+# try:
+#     import majordome as mj
+#     print("\033[34m- Imported majordome as mj\033[0m")
+# except ImportError:
+#     print("\033[31m- Could not import majordome\033[0m")

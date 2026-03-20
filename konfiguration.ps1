b216ab7e499b6b968539b561943798ae84ea9b1f
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-03-20 15:45:15 +0100

    Adding lpm

--- konfiguration.ps1
+++ konfiguration.ps1
@@ -465,6 +465,9 @@ function Invoke-ConfigureLiteXL {
         Set-KompanionEnvVar -Name "LITEXL_HOME" `
             -Value "$env:KOMPANION_BIN\$target"
 
+        $_ = Invoke-DownloadIfNeeded $KOMPANION_SETUP.url.lpm "$env:LITEXL_HOME\lpm.exe"
+        # & "$env:LITEXL_HOME\lpm.exe" install plugin_manager --assume-yes
+
         Initialize-AddToPath -Directory "$env:LITEXL_HOME"
     } else {
         Write-Warn "Failed to install LiteXL, skipping configuration..."

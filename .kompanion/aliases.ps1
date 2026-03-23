577287eda5ac491b598d8688f5845864db9dde42
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-03-23 16:31:59 +0100

    Fix alias

--- .kompanion/aliases.ps1
+++ .kompanion/aliases.ps1
@@ -7,12 +7,18 @@ Set-Alias -Name ll     -Value Get-ChildItem
 Set-Alias -Name which  -Value Get-Command
 
 # Programs
-Set-Alias -Name vim    -Value nvim
 Set-Alias -Name gmsh   -Value gmsh.exe
 
 # Function wrapped
 function Print-Path() { return $env:Path -split ';' }
 function mj { cd $env:KOMPANION_DIR }
+
+function vim {
+    # For some reason nvim is not taking into account the override of the
+    # user profile (APPDATA) and is looking for the config in the default
+    # location. This is a workaround to force it to use the provided config.
+    & nvim.exe -u "$env:KOMPANION_DIR\config\nvim\init.lua" @args
+}
 
 function zettlr {
     & Start-Process Zettlr.exe -ArgumentList "--data-dir=$env:ZETTLR_DATA"
